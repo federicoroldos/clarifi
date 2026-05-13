9466301cabda9f23b72ceaf2b9ff16e7c08c7ac5
Federico <fede212yt@gmail.com>
2026-05-13 16:10:21 -0300

    bump to 0.1.9; fix custom titlebar drag via win32 HTCAPTION

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from openpyxl import Workbook, load_workbook
 from threading import Lock
 import os, sys, secrets, json, urllib.request, urllib.error
 
-APP_VERSION = '0.1.8'
+APP_VERSION = '0.1.9'
 GITHUB_REPO = 'federicoroldos/basic-personal-finances-tracker'
 
 
